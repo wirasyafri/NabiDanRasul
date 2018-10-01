ad014fd50bf2f182aa9d20bb3637db65dce023e2
syafri wira wicaksana <wirasyafri1998@gmail.com>
2018-10-01 20:35:03 +0700

    data nabi v.1.1

--- app/src/main/java/com/ti4e/wiranafi/nabidanrasul/Nabi.java
+++ app/src/main/java/com/ti4e/wiranafi/nabidanrasul/Nabi.java
@@ -3,7 +3,7 @@ package com.ti4e.wiranafi.nabidanrasul;
 public class Nabi {
     private String nama_nabi,deskripsi;
     public static final Nabi[]kisahnabi={
-            new Nabi("Nabi Adam","Nabi pertama " +
+            new Nabi("1. Nabi Adam","Nabi pertama " +
                     "\n Allah Subhanahu wa Ta’ala memberitahukan kepada para malaikat tentang penciptaan Adam ‘alaihis salam, Dia berfirman:\n" +
                     "\n" +
                     "“Sesungguhnya aku hendak menjadikan seorang khalifah di muka bumi.” (QS. Al Baqarah: 30)\n" +
@@ -12,7 +12,7 @@ public class Nabi {
                     "\n" +
                     "\n" +
                     "\n" ),
-            new Nabi("Nabi Idris","Nabi kedua " +
+            new Nabi("2. Nabi Idris","Nabi kedua " +
                     "\n Nabi Idris adalah keturunan keenam dari Nabi Adam as. Dia adalah putra dari Qabil dan Iqlima (putra dan putri Nabi Adam as).\n" +
                     "\n" +
                     "Saat itu, Allah memerintahkan Nabi Idris untuk mengajak seluruh manusia agar berjalan pada kebenaran. Saat itu dia adalah manusia pertama yang menerima wahyu lewat Malaikat Jibril ketika dirinya berusia 82 tahun.\n" +
@@ -26,29 +26,29 @@ public class Nabi {
                     "3. Nama Idris berasal dari kata Darasa yang artinya belajar. Nabi Idris pun kenal sangat senang belajar, dan tekun mengkaji fenomena alam semesta.\n" +
                     "\n" +
                     "4. Nabi Idris adalah orang pertama yang pandai memotong dan menjahit pakaiannya. Orang-orang sebelumnya konon hanya mengenakan kulit binatang sebagai penutup aurat."),
-            new Nabi("Nabi Nuh",""),
-            new Nabi("Nabi Hud",""),
-            new Nabi("Nabi Shaleh",""),
-            new Nabi("Nabi Ibrahim",""),
-            new Nabi("Nabi Luth",""),
-            new Nabi("Nabi Ismail",""),
-            new Nabi("Nabi Ishaq",""),
-            new Nabi("Nabi Yakub",""),
-            new Nabi("Nabi Yusuf",""),
-            new Nabi("Nabi Syu'aib",""),
-            new Nabi("Nabi Ayub",""),
-            new Nabi("Nabi Zulkifli",""),
-            new Nabi("Nabi Musa",""),
-            new Nabi("Nabi Harun",""),
-            new Nabi("Nabi Daud",""),
-            new Nabi("Nabi Sulaiman",""),
-            new Nabi("Nabi Ilyas",""),
-            new Nabi("Nabi Ilyasa",""),
-            new Nabi("Nabi Yunus",""),
-            new Nabi("Nabi Zakaria",""),
-            new Nabi("Nabi Yahya",""),
-            new Nabi("Nabi Isa",""),
-            new Nabi("Nabi Muhammad ","Nabi terakhir dan rasul terakhir"),
+            new Nabi("3. Nabi Nuh",""),
+            new Nabi("4. Nabi Hud",""),
+            new Nabi("5. Nabi Shaleh",""),
+            new Nabi("6. Nabi Ibrahim",""),
+            new Nabi("7. Nabi Luth",""),
+            new Nabi("8. Nabi Ismail",""),
+            new Nabi("9. Nabi Ishaq",""),
+            new Nabi("10. Nabi Yakub",""),
+            new Nabi("11. Nabi Yusuf",""),
+            new Nabi("12. Nabi Syu'aib",""),
+            new Nabi("13. Nabi Ayub",""),
+            new Nabi("14. Nabi Zulkifli",""),
+            new Nabi("15. Nabi Musa",""),
+            new Nabi("16. Nabi Harun",""),
+            new Nabi("17. Nabi Daud",""),
+            new Nabi("18. Nabi Sulaiman",""),
+            new Nabi("19. Nabi Ilyas",""),
+            new Nabi("20. Nabi Ilyasa",""),
+            new Nabi("21. Nabi Yunus",""),
+            new Nabi("22. Nabi Zakaria",""),
+            new Nabi("23. Nabi Yahya",""),
+            new Nabi("24. Nabi Isa",""),
+            new Nabi("25. Nabi Muhammad ","Nabi terakhir dan rasul terakhir"),
 
 
     };
