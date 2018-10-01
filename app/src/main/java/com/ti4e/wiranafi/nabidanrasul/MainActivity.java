0862137d12d6bdad5027037ca695c0ccfefcf285
syafri wira wicaksana <wirasyafri1998@gmail.com>
2018-10-01 19:55:00 +0700

    deskripsi awal

--- app/src/main/java/com/ti4e/wiranafi/nabidanrasul/MainActivity.java
+++ app/src/main/java/com/ti4e/wiranafi/nabidanrasul/MainActivity.java
@@ -1,13 +1,47 @@
 package com.ti4e.wiranafi.nabidanrasul;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ResepListFragment.Listener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+//    public void onShowDetails(View view){
+//        Intent intent = new Intent(this,DetailActivity.class);
+//        startActivity(intent);
+//    }
+
+    public void itemClicked(long id)
+    {
+        View fragmenDetail = findViewById(R.id.fragment_container);
+        if(fragmenDetail != null )
+        {
+            ResepDetailFragment deta = new ResepDetailFragment();
+            FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
+            deta.setResep(id);
+            fragTrans.replace(R.id.fragment_container,deta);
+            fragTrans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+//            supaya tidak ketumpuk transactionnya maka script dibawah ini di comment
+            //fragTrans.addToBackStack(null);
+            fragTrans.commit();
+        }
+
+        else
+        {
+            Toast.makeText(this, "Item " + id + " was clicked", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this.getApplicationContext(), DetailActivity.class);
+            Bundle b = new Bundle();
+            b.putDouble("id",id);
+            intent.putExtras(b);
+            startActivity(intent);
+        }
     }
 }
